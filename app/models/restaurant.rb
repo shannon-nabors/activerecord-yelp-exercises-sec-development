544c25533992fb198328d305e3cc9a14bbff8e19
shannon-nabors <shannon.nabors@gmail.com>
2019-08-21 17:36:33 -0400

    restaurant class methods, sans ones that require redoing seed

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,24 @@ class Restaurant < ActiveRecord::Base
     has_many :dishes
     validates :name, presence: true
 
+    def self.mcdonalds
+        self.find_by(name: "McDonalds")
+    end
 
+    def self.tenth
+        self.find(10)
+    end
+
+    def self.with_long_names
+        self.where("LENGTH(name) > 12")
+    end
+
+    def self.name_like(n)
+        self.where('name LIKE ?', "%#{n}%")
+    end
+
+    def self.name_not_like(n)
+        self.where('name NOT LIKE ?', "%#{n}%")
+    end
 
 end
