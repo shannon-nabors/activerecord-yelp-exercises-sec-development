809a077818b45e22b40a6b6df67ad3a3921db25b
shannon-nabors <shannon.nabors@gmail.com>
2019-08-21 12:40:56 -0400

    created migrations

---
+++ db/migrate/20190820210838_create_restaurants_table.rb
@@ -0,0 +1,7 @@
+class CreateRestaurantsTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+    end
+  end
+end
---
+++ db/migrate/20190820210855_create_dishes_table.rb
@@ -0,0 +1,8 @@
+class CreateDishesTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :dishes do |t|
+      t.string :name
+      t.integer :restaurant_id
+    end
+  end
+end
