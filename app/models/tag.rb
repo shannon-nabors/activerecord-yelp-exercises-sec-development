32237ddcd5a12a676e122bae027235437476e562
shannon-nabors <shannon.nabors@gmail.com>
2019-08-23 09:32:21 -0400

    fixed error on attempted validation of nonexistent tag name

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,11 +1,13 @@
 class Tag < ActiveRecord::Base
+    has_many :dish_tags
+    has_many :dishes, through: :dish_tags
     validates :name, length: { minimum: 3, too_short: "too short" }
     validate :two_words_max
 
     private
     
     def two_words_max
-        if self.name.split.length > 2
+        if self.name && self.name.split.length > 2
             errors.add(:too_long, "Tag must be one or two words")
         end
     end
