038b9d33b70afc901cb4f91177d933558714e073
shannon-nabors <shannon.nabors@gmail.com>
2019-08-23 09:28:46 -0400

    changed seed to randomize number of dishes and tags for restaurants and dishes, respectively

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,24 @@ Tag.destroy_all
 DishTag.destroy_all
 
 restaurants = ["Chipotle", "Strapazza", "Columbia Alehouse", "Eggspectations", "Chopt", "KFC", "La Malinche", "McDonalds", "Lebanese Taverna", "Roti", "District Taco", "Cava", "Mod Pizza", "Red Lobster", "Hunan Manor", "House of India", "Bollywood Masala", "La Palapa Too", "Uncle Julio's", "McGinty's"]
-
 dishes = ["Hamburger", "Fish Tacos", "Sesame Chicken", "Chicken Korma", "Falafel", "Dumplings", "Reuben Sandwich", "French Fries", "Rice Bowl", "Fish and Chips", "Chicken Tikka Masala", "Samosas", "Chicken Sandwich", "Patatas Bravas", "Vegetable Omelet", "Spaghetti", "Caesar Salad", "Spinach Ravioli", "Steak Fajitas", "Pepperoni Pizza", "Fried Rice", "Cheese Pizza", "Palak Paneer", "Drumsticks", "Shrimp Scampi", "Chips and Guacamole"]
-
 tags = ["Vegetarian", "Spicy", "Side Dish", "Breakfast", "Seafood", "Italian", "Indian", "Fast Food", "Small Plates", "Sandwiches", "Pizza", "Classics", "Healthy", "Pub Fare", "Shareable", "Appetizer", "Dessert", "Main Dish"]
+
+r = Random.new
 
 restaurants.each { |n| Restaurant.create(name: n) }
 
-Restaurant.all.each do |r|
-    dishes.sample(10).each do |dish|
-        Dish.create(name: dish, restaurant_id: r.id)
+Restaurant.all.each do |restaurant|
+    dishes.sample(r.rand(1..25)).each do |dish|
+        Dish.create(name: dish, restaurant_id: restaurant.id)
     end
 end
 
 tags.each { |n| Tag.create(name: n) }
 
-Dish.all.each do |d|
-    Tag.all.sample(3).each do |t|
-        DishTag.create(dish_id: d.id, tag_id: t.id)
+Dish.all.each do |dish|
+    Tag.all.sample(r.rand(0..5)).each do |tag|
+        DishTag.create(dish_id: dish.id, tag_id: tag.id)
     end
 end
 
