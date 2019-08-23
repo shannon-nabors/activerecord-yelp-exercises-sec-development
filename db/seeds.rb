d9a0062f49e042a5c6a23b480eee61a66e5db413
shannon-nabors <shannon.nabors@gmail.com>
2019-08-23 14:35:28 -0400

    added apostrophe to McDonald's

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ Dish.destroy_all
 Tag.destroy_all
 DishTag.destroy_all
 
-restaurants = ["Chipotle", "Strapazza", "Columbia Alehouse", "Eggspectations", "Chopt", "KFC", "La Malinche", "McDonalds", "Lebanese Taverna", "Roti", "District Taco", "Cava", "Mod Pizza", "Red Lobster", "Hunan Manor", "House of India", "Bollywood Masala", "La Palapa Too", "Uncle Julio's", "McGinty's"]
+restaurants = ["Chipotle", "Strapazza", "Columbia Alehouse", "Eggspectations", "Chopt", "KFC", "La Malinche", "McDonald's", "Lebanese Taverna", "Roti", "District Taco", "Cava", "Mod Pizza", "Red Lobster", "Hunan Manor", "House of India", "Bollywood Masala", "La Palapa Too", "Uncle Julio's", "McGinty's"]
 dishes = ["Hamburger", "Fish Tacos", "Sesame Chicken", "Chicken Korma", "Falafel", "Dumplings", "Reuben Sandwich", "French Fries", "Rice Bowl", "Fish and Chips", "Chicken Tikka Masala", "Samosas", "Chicken Sandwich", "Patatas Bravas", "Vegetable Omelet", "Spaghetti", "Caesar Salad", "Spinach Ravioli", "Steak Fajitas", "Pepperoni Pizza", "Fried Rice", "Cheese Pizza", "Palak Paneer", "Drumsticks", "Shrimp Scampi", "Chips and Guacamole"]
 tags = ["Vegetarian", "Spicy", "Side Dish", "Breakfast", "Seafood", "Italian", "Indian", "Fast Food", "Small Plates", "Sandwiches", "Pizza", "Classics", "Healthy", "Pub Fare", "Shareable", "Appetizer", "Dessert", "Main Dish"]
 
