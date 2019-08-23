dc7a693574db90a8da7cfeaccd444eb1bccaf866
shannon-nabors <shannon.nabors@gmail.com>
2019-08-23 09:33:16 -0400

    moved validation of dish_tag uniqueness onto dish model to match existing specs

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -4,4 +4,12 @@ class Dish < ActiveRecord::Base
     has_many :tags, through: :dish_tags
     validates :name, presence: true
     validates :restaurant_id, presence: true
+    validate :no_duplicate_tags
+
+    def no_duplicate_tags
+        if self.tags != self.tags.uniq
+            errors.add(:tags, "A dish can't have two of the same tag")
+        end
+    end
+    
 end
--- app/models/dish_tag.rb
+++ app/models/dish_tag.rb
@@ -1,4 +1,5 @@
 class DishTag < ActiveRecord::Base
     belongs_to :tag 
     belongs_to :dish
+    # validates :dish, uniqueness: { scope: :tag }
 end
