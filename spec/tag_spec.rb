c5271bf861c462e29cf17077588d7bcf3e4f197a
shannon-nabors <shannon.nabors@gmail.com>
2019-08-27 17:22:15 -0400

    added tests for Tag instance methods

--- spec/tag_spec.rb
+++ spec/tag_spec.rb
@@ -54,7 +54,7 @@ describe 'Tag' do
     let(:calamari) { Dish.create(name: "Calamari", restaurant: la_malinche) }
     let(:olives) { Dish.create(name: "Olives", restaurant: la_malinche) }
 
-    it ".most_common returns the tag with the most associated dishes" do
+    it "Tag.most_common returns the tag with the most associated dishes" do
       datiles.tags << tapas
       patatas.tags << [vegetarian, tapas]
       empanada.tags << [tapas, spicy]
@@ -62,7 +62,7 @@ describe 'Tag' do
       expect(Tag.most_common).to eq(tapas)
     end
 
-    it ".least_common returns the tag with the least associated dishes" do
+    it "Tag.least_common returns the tag with the least associated dishes" do
       datiles.tags << tapas
       patatas.tags << [vegetarian, tapas, spicy]
       empanada.tags << [tapas, spicy]
@@ -70,7 +70,7 @@ describe 'Tag' do
       expect(Tag.least_common).to eq(vegetarian)
     end
 
-    it ".unused returns all tags that haven't been used" do
+    it "Tag.unused returns all tags that haven't been used" do
       tapas = Tag.create(name:"Tapas")
       spicy = Tag.create(name:"Spicy")
       vegetarian = Tag.create(name:"Vegetarian")
@@ -85,7 +85,7 @@ describe 'Tag' do
       expect(Tag.unused).to eq([vegetarian])
     end
 
-    it ".uncommon returns all tags that have been used fewer than five times" do
+    it "Tag.uncommon returns all tags that have been used fewer than five times" do
       datiles.tags << tapas
       patatas.tags << [vegetarian, tapas, spicy]
       empanada.tags << [tapas, spicy]
@@ -101,7 +101,7 @@ describe 'Tag' do
       expect(Tag.uncommon).to eq([vegetarian])
     end
 
-    it ".popular returns the top 5 tags by frequency of use" do
+    it "Tag.popular returns the top 5 tags by frequency of use" do
       seafood = Tag.create(name: "Seafood")
       shareable = Tag.create(name: "Shareable")
       lowcarb = Tag.create(name: "Low Carb")
@@ -118,7 +118,49 @@ describe 'Tag' do
 
   end
 
-  # describe "Instance methods:" do
-  # end
+  describe "Instance methods:" do
+    let(:la_malinche) { Restaurant.create(name: "La Malinche") }
+    let(:mcgintys) { Restaurant.create(name: "McGinty's") }
+    let(:vegetarian) { Tag.create(name: "Vegetarian") }
+    let(:tapas) { Tag.create(name: "Tapas") }
+    let(:seafood) { Tag.create(name: "Seafood") }
+    let(:patatas) { Dish.create(name: "Patatas Bravas", restaurant: la_malinche) }
+    let(:calamari) { Dish.create(name: "Calamari", restaurant: la_malinche) }
+    let(:fish_and_chips) { Dish.create(name: "Fish and Chips", restaurant: mcgintys) }
+    let(:olives) { Dish.create(name: "Olives", restaurant: la_malinche) }
+    let(:fries) { Dish.create(name: "French Fries", restaurant: mcgintys) }
+
+    it "Tag#restaurants returns any restaurants that have a dish with this tag" do
+      patatas.tags << [tapas, vegetarian]
+      calamari.tags << [tapas, seafood]
+      fish_and_chips.tags << seafood
+      
+      expect(tapas.restaurants).to eq([la_malinche])
+      expect(seafood.restaurants.length).to eq([2])
+    end
+
+    it "Tag#top_restaurant returns the restaurant that uses this tag the most" do
+      patatas.tags << [tapas, vegetarian]
+      calamari.tags << [tapas, seafood]
+      olives.tags << [tapas, vegetarian]
+      fish_and_chips.tags << seafood
+      fries.tags << vegetarian
+
+      expect(vegetarian.top_restaurant).to eq(la_malinche)
+    end
+
+    it "Tag#dish_count returns the number of dishes that use this tag" do
+      patatas.tags << [tapas, vegetarian]
+      calamari.tags << [tapas, seafood]
+      olives.tags << [tapas, vegetarian]
+      fish_and_chips.tags << seafood
+      fries.tags << vegetarian
+
+      expect(vegetarian.dish_count).to eq(3)
+      expect(seafood.dish_count).to eq(2)
+      expect(tapas.dish_count).to eq(3)
+    end
+
+  end
   
 end
