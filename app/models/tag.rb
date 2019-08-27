2cb8a5e817b51609074100658eb51ef35762eb99
shannon-nabors <shannon.nabors@gmail.com>
2019-08-27 14:42:30 -0400

    Tag class methods passing

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -10,6 +10,28 @@ class Tag < ActiveRecord::Base
     validates :name, length: { minimum: 3, too_short: "too short" }
     validate :two_words_max
 
+    ################################# Scopes #####################################
+
+    scope :unused, -> { eager_load(:dishes).where("dishes.id IS NULL") }
+
+    ############################# Class Methods ##################################
+
+    def self.most_common
+        joins(:dishes).group("tag_id").order(Arel.sql("count(dish_id) DESC LIMIT 1"))[0]
+    end
+
+    def self.least_common
+        joins(:dishes).group("tag_id").order(Arel.sql("count(dish_id) ASC LIMIT 1"))[0]
+    end
+
+    def self.uncommon
+        joins(:dishes).group("tag_id").having("count(dish_id) < ?", 5)
+    end
+
+    def self.popular
+        joins(:dishes).group("tag_id").order(Arel.sql("count(dish_id) DESC LIMIT 5"))
+    end
+
     ############################# Private Methods ################################
 
     private
